34dded1e2080d3632e484ec9821cfdc473041d70
Branislav Goga <Branislav Goga@BRANO.fox.tatramed.sk>
2018-05-21 17:46:10 +0200

    Fix MySQL visibility from other containers. Add Adminer.

--- source/index.php
+++ source/index.php
@@ -8,13 +8,23 @@
         <h1>Hello World!</h1>
         <div>
             <?php
-                echo date("d.m.Y H:m:s");
+            
+                $newline = "<br>";
+            
+                echo date("d.m.Y H:m:s").$newline;
                 
                 //phpinfo();
                 
-                $database   = $user = $password = "project";
-                $host       = "mysql";
-                $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
+                $database   = $user = $password = "dockerweb";
+                $host       = "db";
+                
+                $connectionstring = "mysql:host={$host};dbname={$database};charset=utf8";
+                
+                echo    "Connection string: $connectionstring".$newline;
+                echo    "User: $user".$newline;
+                echo    "Password: $password".$newline;
+                
+                $connection = new PDO($connectionstring, $user, $password);
                 $query      = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");
                 $tables     = $query->fetchAll(PDO::FETCH_COLUMN);
 
